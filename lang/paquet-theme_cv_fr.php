edf0c3f791e7b78b7aa5ed4fb70a489adf042217
Sylvain Lesage <severo@rednegra.net>
2013-05-12 17:18:14 +0200

    Création du thème

---
+++ lang/paquet-theme_cv_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-theme_cv
+// Langue: fr
+// Date: 12-05-2013 17:16:34
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// T
+	'theme_cv_description' => 'Un thème de test, sur 2 colonnes fixes.',
+	'theme_cv_slogan' => 'Thème Z de CV',
+);
+?>
